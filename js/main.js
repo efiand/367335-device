b4209ca946dcaa88b60ad5b84cc45aa02cec6cc3
Andrey Efimov <efiand@ya.ru>
2018-03-25 00:35:20 +0300

    Валидация, структура заголовков и проверка на наличие объекта в скрипте

--- js/main.js
+++ js/main.js
@@ -2,23 +2,31 @@
 var mapOnBtn = document.querySelector('.contacts__map-link');
 var mapOffBtn = document.querySelector('.contacts__close--map');
 var mapContainer = document.getElementById('map');
-mapOnBtn.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  mapContainer.classList.add('map--js');
-});
-mapOffBtn.addEventListener('click', function(evt) {
-  mapContainer.classList.remove('map--js');
-});
+if (mapOnBtn) {
+  mapOnBtn.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    mapContainer.classList.add('map--js');
+  });
+}
+if (mapOffBtn) {
+  mapOffBtn.addEventListener('click', function(evt) {
+    mapContainer.classList.remove('map--js');
+  });
+}
 
 /** Форма обратной связи */
 var feedbackOnBtn = document.querySelector('.info__link--feedback');
 var feedbackOffBtn = document.querySelector('.contacts__close--feedback');
 var feedbackContainer = document.getElementById('feedback');
-feedbackOnBtn.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  feedbackContainer.classList.add('feedback--js');
-});
-feedbackOffBtn.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  feedbackContainer.classList.remove('feedback--js');
-});
+if (feedbackOnBtn) {
+  feedbackOnBtn.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    feedbackContainer.classList.add('feedback--js');
+  });
+}
+if (feedbackOffBtn) {
+  feedbackOffBtn.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    feedbackContainer.classList.remove('feedback--js');
+  });
+}
