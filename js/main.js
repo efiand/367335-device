754bc63a9a2aa39150ad4c5ea0670b1e9605d1c8
Andrey Efimov <efiand@ya.ru>
2018-03-25 23:08:50 +0300

    Пагинация и JS

--- js/main.js
+++ js/main.js
@@ -30,25 +30,6 @@ if (mapContainer) {
 }
 
 
-/** Форма обратной связи */
-var feedbackContainer = document.getElementById('feedback');
-
-if (feedbackContainer) {
-  var feedbackOnBtn = document.querySelector('.info__link--feedback');
-  var feedbackOffBtn = document.querySelector('.contacts__close--feedback');
-
-  feedbackOnBtn.addEventListener('click', function(evt) {
-    evt.preventDefault();
-    feedbackContainer.classList.add('feedback--js');
-  });
-
-  feedbackOffBtn.addEventListener('click', function(evt) {
-    evt.preventDefault();
-    feedbackContainer.classList.remove('feedback--js');
-  });
-}
-
-
 /** Фиксация выпадающего меню по кнопке */
 var menuOnBtn = document.querySelector('.nav__spoiler-btn--on');
 var menuOffBtn = document.querySelector('.nav__spoiler-btn--off');
@@ -81,37 +62,117 @@ menuOffBtn.addEventListener('click', function() {
 });
 
 
-/** Оживление слайдера */
-var minField = document.querySelector('.choose-cost__field--min');
+/** Прогрессивное улучшение формы поиска */
+var searchForm = document.querySelector('.search');
+var searchField = document.querySelector('.search__field');
 
-if (minField) {
-  var maxField = document.querySelector('.choose-cost__field--max');
-  var minPoint = document.querySelector('.choose-cost__point--min');
-  var maxPoint = document.querySelector('.choose-cost__point--max');
-  var range = document.querySelector('.choose-cost__range');
-
-  minField.addEventListener('change', function() {
-    rangeSlider(minField, minPoint);
-  });
-
-  maxField.addEventListener('change', function() {
-    rangeSlider(maxField, maxPoint);
-  });
-
-  function rangeSlider(field, point) {
-    var valueLeft = field.value / 44 - 5;
-    var valueLeftAlt = minPoint.style.left.replace('px', '');
-    if (field == minField) {
-      valueLeftAlt = maxPoint.style.left.replace('px', '');
-    }
-    point.style.left = valueLeft + 'px';
-    var leftRange = valueLeft;
-    var rightRange = valueLeftAlt;
-    if (valueLeft > valueLeftAlt) {
-      leftRange = valueLeftAlt;
-      rightRange = valueLeft;
-    }
-    range.style.left = leftRange + 'px';
-    range.style.right = (201 - rightRange) + 'px';
+searchForm.addEventListener('submit', function(evt) {
+  evt.preventDefault();
+  searchField.value = searchField.value.trim();
+  if (searchField.classList.contains('search__field--js')) {
+    searchField.classList.remove('search__field--js');
   }
+  if (searchField.value) {
+    searchForm.submit();
+  }
+  else {
+    searchField.classList.add('search__field--js');
+  }
+});
+
+
+/** Прогрессивное улучшение формы обратной связи */
+var feedbackForm = document.getElementById('feedback');
+
+if (feedbackForm) {
+  var feedbackOnBtn = document.querySelector('.info__link--feedback');
+  var feedbackOffBtn = document.querySelector('.contacts__close--feedback');
+  var feedbackName = document.getElementById('name');
+  var feedbackMail = document.getElementById('email');
+  var feedbackText = document.getElementById('text');
+
+  feedbackOnBtn.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    feedbackForm.classList.add('feedback--js');
+    feedbackName.value = localStorage.getItem('name');
+    feedbackMail.value = localStorage.getItem('email');
+    feedbackText.value = localStorage.getItem('text');
+    if (!feedbackName.value) {
+      feedbackName.focus();
+    }
+    else if (!feedbackMail.value) {
+      feedbackMail.focus();
+    }
+    else {
+      feedbackText.focus();
+    }
+  });
+
+  feedbackName.addEventListener('blur', function(evt) {
+    if (feedbackName.classList.contains('feedback__field--js')) {
+      feedbackName.classList.remove('feedback__field--js');
+    }
+    feedbackName.value = feedbackName.value.trim();
+
+    var namePattern = /[А-Я]{1}[а-я]+ [А-Я]{1}[а-я]+/;
+    if (!namePattern.test(feedbackName.value)) {
+      feedbackName.classList.add('feedback__field--js');
+      feedbackName.value = '';
+      feedbackName.focus();
+    }
+    else {
+      localStorage.setItem('name', feedbackName.value);
+    }
+  });
+
+  feedbackMail.addEventListener('blur', function(evt) {
+    if (feedbackMail.classList.contains('feedback__field--js')) {
+      feedbackMail.classList.remove('feedback__field--js');
+    }
+    feedbackMail.value = feedbackMail.value.trim();
+
+    var mailPattern = /.+@.+\..+/i;
+    if (!mailPattern.test(feedbackMail.value)) {
+      feedbackMail.classList.add('feedback__field--js');
+      feedbackMail.value = '';
+      feedbackMail.focus();
+    }
+    else {
+      localStorage.setItem('email', feedbackMail.value);
+    }
+  });
+
+  feedbackText.addEventListener('blur', function(evt) {
+    if (feedbackText.classList.contains('feedback__field--js')) {
+      feedbackText.classList.remove('feedback__field--js');
+    }
+    feedbackText.value = feedbackText.value.trim();
+    if (!feedbackText.value) {
+      feedbackText.classList.add('feedback__field--js');
+      feedbackText.value = '';
+      feedbackText.focus();
+    }
+    else {
+      localStorage.setItem('text', feedbackText.value);
+    }
+  });
+
+  feedbackForm.addEventListener('submit', function(evt) {
+    evt.preventDefault();
+    feedbackForm.classList.remove('feedback--js')
+    feedbackForm.submit();
+  });
+
+  feedbackForm.addEventListener('keydown', function(evt) {
+    if (evt.keyCode === 27) {
+      if (feedbackForm.classList.contains('feedback--js')) {
+        feedbackForm.classList.remove('feedback--js');
+      }
+    }
+  });
+
+  feedbackOffBtn.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    feedbackForm.classList.remove('feedback--js');
+  });
 }
